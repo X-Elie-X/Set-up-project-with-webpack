af19986c116d425d18fa2a71ad88b49126024086
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-08-09 18:39:10 +0200

    Webpack project

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
